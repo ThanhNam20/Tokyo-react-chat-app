3c5fa4005750074c8efbda2cb4bb82cd6ad27e43
ThanhNam20 <khuatthanhnam@gmail.com>
2020-06-09 22:55:26 +0700

    Factories fn

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,50 +1,43 @@
-import React, { Component } from 'react'
-import io from 'socket.io-client'
-import { USER_CONNECTED,LOGOUT } from '../Events';
-const socketUrl = "http://192.168.41.100:8080"
+import React, { Component } from "react";
+import io from "socket.io-client";
+import { USER_CONNECTED, LOGOUT } from "../Events";
+const socketUrl = "http://192.168.41.100:8080";
 
 export default class Layout extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            socket: null,
-            user:null
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      socket: null,
+      user: null,
+    };
+  }
 
-    componentWillMount() {
-        this.initSocket();
-    }
+  componentWillMount() {
+    this.initSocket();
+  }
 
-    initSocket = () => {
-        const socket = io(socketUrl);
+  initSocket = () => {
+    const socket = io(socketUrl);
+    socket.on("connect", () => {
+      console.log("Connected");
+    });
+    this.setState({ socket });
+  };
 
-        socket.on('connect', () => {
-            console.log("Connected")
-        });
-        this.setState({
-            socket
-        })
-    }
+  setUser = (user) => {
+    const { socket } = this.state;
+    socket.emit(USER_CONNECTED);
+    this.setState({ user });
+  };
 
-    setUser = (user) => {
-        const { socket } = this.state;
-        socket.emit(USER_CONNECTED);
-        this.setState({user})
-    }
+  logout = () => {
+    const { socket } = this.state;
+    socket.emit(LOGOUT);
+    this.setState({ user: null });
+  };
 
-    logout = () => {
-        const { socket } = this.state;
-        socket.emit(LOGOUT)
-        this.setState({ user: null })
-    }
-
-    render() {
-        const { title } = this.props;
-        return (
-            <div className ="container">
-                {title}
-            </div>
-        )
-    }
+  render() {
+    const { title } = this.props;
+    return <div className="container">{title}</div>;
+  }
 }
