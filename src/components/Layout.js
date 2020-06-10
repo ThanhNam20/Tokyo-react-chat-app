ddde6979dd9de26462ac274c0b473df46dfb720c
ThanhNam20 <khuatthanhnam@gmail.com>
2020-06-11 00:43:22 +0700

    Chatroom Container

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import io from "socket.io-client";
 import { USER_CONNECTED, LOGOUT } from "../Events";
 import LoginForm from "./LoginForm";
+import ChatContainer from "./chats/ChatContainer";
 
 const socketUrl = "http://192.168.41.100:8080";
 
@@ -28,7 +29,7 @@ export default class Layout extends Component {
 
   setUser = (user) => {
     const { socket } = this.state;
-    socket.emit(USER_CONNECTED,user);
+    socket.emit(USER_CONNECTED, user);
     this.setState({ user });
   };
 
@@ -39,11 +40,15 @@ export default class Layout extends Component {
   };
 
   render() {
-    // const { title } = this.props;
-    const { socket } = this.state;
+    const { title } = this.props;
+    const { socket, user } = this.state;
     return (
       <div className="container">
-        <LoginForm socket={socket} setUser={this.setUser} />
+        {!user ? 
+          <LoginForm socket={socket} setUser={this.setUser} />
+         : 
+          <ChatContainer socket={socket} user={user} logout={this.logout} />
+        }
       </div>
     );
   }
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,44 +1,47 @@
 import React, { Component } from "react";
 import { VERIFY_USER } from "../Events";
+
 export default class LoginForm extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       nickname: "",
       error: "",
     };
   }
 
-    setUser = ({ user, isUser }) => {
+  setUser = ({ user, isUser }) => {
     if (isUser) {
       this.setError("User name taken");
     } else {
+      this.setError("");
       this.props.setUser(user);
     }
   };
 
-  handleSubmit = (event) => {
-    event.preventDefault();
+  handleSubmit = (e) => {
+    e.preventDefault();
     const { socket } = this.props;
     const { nickname } = this.state;
     socket.emit(VERIFY_USER, nickname, this.setUser);
   };
 
-  handleChange = (event) => {
-    this.setState({ nickname: event.target.value });
+  handleChange = (e) => {
+    this.setState({ nickname: e.target.value });
   };
 
   setError = (error) => {
-    this.setState = { error };
+    this.setState({ error });
   };
 
   render() {
     const { nickname, error } = this.state;
     return (
       <div className="login">
-        <form className="login-form" onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleSubmit} className="login-form">
           <label htmlFor="nickname">
-            <h2>Get a NickName</h2>
+            <h2>Got a nickname?</h2>
           </label>
           <input
             ref={(input) => {
@@ -48,7 +51,7 @@ export default class LoginForm extends Component {
             id="nickname"
             value={nickname}
             onChange={this.handleChange}
-            placeholder={"My UserName"}
+            placeholder={"username"}
           />
           <div className="error">{error ? error : null}</div>
         </form>
