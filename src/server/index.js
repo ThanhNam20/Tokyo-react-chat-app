a88ec2b73b78631cad1815e3aa53ac89a052839c
ThanhNam20 <khuatthanhnam@gmail.com>
2020-06-12 01:20:55 +0700

    Upload to Heroku

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,15 @@
-var app = require('http').createServer();
-var io = module.exports.io = require('socket.io')(app);
+
+const express = require("express");
+const app = express();
+const server = require('http').Server(app)
+const io = module.exports.io = require('socket.io')(server);
 const PORT = process.env.PORT || 8080;
 const socketManager = require('./SocketManager');
+
+app.use(express.static(__dirname+ '/../../build'))
+
 io.on('connection', socketManager);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
 })
